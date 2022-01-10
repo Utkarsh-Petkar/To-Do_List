ba974f70da05deb55055630f421ff3b764497705
Utkarsh\UtkarshP <petkarutkarsh@gmail.com>
2022-01-11 01:03:46 +0530

    To-Do List with React

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+import { useState } from "react";
+import List from './List';
+function App() {
+  const [currentItem, setCurrentItem] = useState(null);
+  const [itemList, updateItemList] = useState([]);
+  const onChangeHandler = (e) => {
+    setCurrentItem(e.target.value);
+  };
+  const addItemToList = () => {
+    updateItemList([...itemList, { item: currentItem, key: Date.now() }]);
+    setCurrentItem("");
+  };
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="Wrapper">
+          <div className="Input-wrapper">
+            <input placeholder="Add Item" value={currentItem} onChange={onChangeHandler} />
+            <button onClick={addItemToList}><b>+</b></button>
+          </div>
+          <List itemList={itemList} updateItemList={updateItemList} />
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
